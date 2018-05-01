0eec32c2ece78f86100e96add723ef0936aea567
z30832564 <630832564@qq.com>
2018-05-01 13:35:42 +0800

    Add files via upload

--- fushijshi/qinghua-chengjipaiming/1/main.cpp
+++ fushijshi/qinghua-chengjipaiming/1/main.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int main(int argc, char** argv) {
+	int n, f, i,j, fenshu;
+	string ming="";
+	vector<string> name[101];
+	while (cin >> n >> f){
+	for(i=0;i<n;i++)
+	{
+		cin>>ming>>fenshu;
+		name[fenshu].push_back(ming);
+	}
+	if(f==1){
+		for(i=0;i<=100;i++)
+		{
+			if(name[i].size()>0){
+			
+				for(j=0;j<name[i].size();j++)
+				{
+					cout<< name[i][j] <<" " <<i<<endl;
+				}
+			}
+		}
+	}
+	else{
+		for(i=100;i>=0;i--)
+		{
+			if(name[i].size()>0){
+			
+				for(j=0;j<name[i].size();j++)
+				{
+					cout<< name[i][j] <<" " <<i<<endl;
+				}
+			}
+		}
+	}
+}
+	return 0;
+}
